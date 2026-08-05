e6ca83d3eb579ea2baeaa5d19583895fa6d2248b
Saritha Tharani <saritha.tharani71@gmail.com>
2026-08-05 21:36:03 +0530

    Add GFG Largest Element

---
+++ gfg/Largest-Element-in-Array/LargestElement.java
@@ -0,0 +1,9 @@
+class Solution {
+    int largest(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) max = arr[i];
+        }
+        return max;
+    }
+}
